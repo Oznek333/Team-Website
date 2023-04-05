50f6b2f0ed564f25dd5a209c9514536cbb2d122b
Oznek333 <robin.reinders@ziggo.nl>
2023-04-05 11:18:48 +0200

    sdf
    alles

--- team.php
+++ team.php
@@ -19,22 +19,22 @@
     <div class="container mt-3">
         <div class="row text-white">
             <div class="col-6 col-sm-3 animate__animated animate__fadeIn text-center">
-                <a class="ripple" href="#!"><img style="width: 100%" class="rounded-pill" src="assets/images/Tim-van-Kortenhof.jpg" alt="" srcset=""></a>
+                <a class="ripple" href="https://www.linkedin.com/in/tim-van-kortenhof/"><img style="width: 100%" class="rounded-pill" src="assets/images/Tim-van-Kortenhof.jpg" alt="" srcset=""></a>
                 <h4 class="mt-3">Tim van Kortenhof</h4>
                 <p>CEO</p>
             </div>
             <div class="col-6 col-sm-3 animate__animated animate__fadeIn text-center">
-                <a class="ripple" href="#!"><img style="width: 100%" class="rounded-pill" src="assets/images/Robin-Reinders.jpg" alt="" srcset=""></a>
+                <a class="ripple" href="https://www.linkedin.com/in/robin-reinders-3b98b5223/"><img style="width: 100%" class="rounded-pill" src="assets/images/Robin-Reinders.jpg" alt="" srcset=""></a>
                 <h4 class="mt-3">Robin Reinders</h4>
                 <p>COO</p>
             </div>
             <div class="col-6 col-sm-3 animate__animated animate__fadeIn text-center">
-                <a class="ripple" href="#!"><img style="width: 100%" class="rounded-pill" src="assets/images/Jurre-Michielsen.jpg" alt="" srcset=""></a>
+                <a class="ripple" href="https://www.linkedin.com/in/jurre-michielsen-112800223/"><img style="width: 100%" class="rounded-pill" src="assets/images/Jurre-Michielsen.jpg" alt="" srcset=""></a>
                 <h4 class="mt-3">Jurre Michielsen</h4>
                 <p>Marketing Agency</p>
             </div>
             <div class="col-6 col-sm-3 animate__animated animate__fadeIn text-center">
-                <a class="ripple" href="#!"><img style="width: 100%" class="rounded-pill" src="assets/images/Jelle-Langeveld.jpeg" alt="" srcset=""></a>
+                <a class="ripple" href="https://www.linkedin.com/in/jelle-langeveld-8a060b223/"><img style="width: 100%" class="rounded-pill" src="assets/images/Jelle-Langeveld.jpeg" alt="" srcset=""></a>
                 <h4 class="mt-3">Jelle Langeveld</h4>
                 <p>Administratie</p>
             </div>
